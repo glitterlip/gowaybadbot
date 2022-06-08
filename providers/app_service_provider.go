a99b375d41dfb13f195c8a0a5c4abc2834b3f99b
glitter <worldtongfb@gmail.com>
2022-06-08 08:51:49 +0800

    fix(app): real ip log

--- providers/app_service_provider.go
+++ providers/app_service_provider.go
@@ -3,13 +3,20 @@ package providers
 import (
 	"github.com/coocood/freecache"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"goawaybot/store"
 	"runtime/debug"
 )
 
 func RegisterAppServiceProvider(e *echo.Echo) {
-	e.IPExtractor = echo.ExtractIPDirect()
-
+	e.IPExtractor = echo.ExtractIPFromXFFHeader()
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: `{"time":"${time_custom}","id":"${id}","remote_ip":"${remote_ip}",` +
+			`"method":"${method}","uri":"${uri}","user_agent":"${user_agent}",` +
+			`"status":${status},"error":"${error}","latency_human":"${latency_human}"` +
+			`,"bytes_in":${bytes_in},"bytes_out":${bytes_out}}` + "\n",
+		CustomTimeFormat: "01-02 15:04:05",
+	}))
 	RegisterCacheService()
 }
 func RegisterCacheService() {
